30e9402ef42298acd4c03edd48ac4750b5338617
silentz <mepershin@gmail.com>
2021-04-24 16:12:22 +0300

    making top level package for relative import

---
+++ tests/widerface_test.py
@@ -0,0 +1,23 @@
+"""
+Test suite for `model/widerface.py` module.
+"""
+
+# ==================== [IMPORT] ====================
+
+import pytest
+
+from .model.widerface import (
+    WIDERFACEImage,
+    WIDERFACEDataset,
+)
+
+# ===================== [TEST] =====================
+
+
+class TestWIDERFACEImage:
+    pass
+
+
+class TestWIDERFACEDataset:
+    pass
+
